c162bb6c8776990055a1966f72d806aa415c5709
Tojoandri <117723034+Tojoandri@users.noreply.github.com>
2022-11-16 13:07:49 +0300

    Data Frame

--- R_pr_Beginner.R
+++ R_pr_Beginner.R
@@ -1,3 +1,4 @@
+#### Beginner ####
 5+6
 a <- 5
 b <- 6
@@ -5,12 +6,20 @@ a + b
 sum(a,b)
 name <- c ("Bill","Bob")
 name
+#### Exo variable ####
 aa <- 4
 bb <- 7
 sum(aa,bb)
 cc <- sum(aa,bb)
 cc
 dd <- cc / aa 
+cc <- c(aa+bb)
+#cc <- c(aa+bb)this is unecessary
 dd
 dd - bb
 dd
+#### Data Frame ####
+Names <- c ("Nomena","Kanto","Maya","Valery","Tojo")
+Female <- c (TRUE,TRUE,TRUE,FALSE,FALSE)
+Height <- c (1.60,1.52,1.51,1.67,1.71)
+mbc <- data.frame(Names,Female,Height)
