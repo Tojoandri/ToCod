4efafc6c128e44ffbfa2dd4fdcd6315e9f62ca9f
Tojoandri <117723034+Tojoandri@users.noreply.github.com>
2022-11-17 12:13:38 +0300

    Import CSV File

--- R_pr_Beginner.R
+++ R_pr_Beginner.R
@@ -23,3 +23,7 @@ Names <- c ("Nomena","Kanto","Maya","Valery","Tojo")
 Female <- c (TRUE,TRUE,TRUE,FALSE,FALSE)
 Height <- c (1.60,1.52,1.51,1.67,1.71)
 mbc <- data.frame(Names,Female,Height)
+
+read.csv("C:/Users/ANDRIANOME/Documents/Data Frame.csv",sep = ";")
+Data_Doc <- read.csv("C:/Users/ANDRIANOME/Documents/Data Frame.csv",sep = ";")
+read.csv2("C:/Users/ANDRIANOME/Documents/Data Frame.csv")
